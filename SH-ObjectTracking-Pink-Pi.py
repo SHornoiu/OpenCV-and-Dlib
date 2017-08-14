a2124e6e012d047a5c2dc6ab71cf5b41b7c16dd6
SHornoiu <sorin.hornoiu@lafargeholcim.com>
2017-08-15 00:09:56 +0300

    Correction for RPI3
    Correction for RPI3

--- SH-ObjectTracking-Pink-Pi.py
+++ SH-ObjectTracking-Pink-Pi.py
@@ -5,30 +5,31 @@ import os
 
 
 # This system command loads the right drivers for the Raspberry Pi camera
-#os.system('sudo modprobe bcm2835-v4l2')
+os.system('sudo pkill uv4l')
+os.system('sudo modprobe bcm2835-v4l2')
 
 w=480
 h=320
 
 my_camera = cv2.VideoCapture(0)
-my_camera.set(3,w)
-my_camera.set(4,h)
+#my_camera.set(3,w)
+#my_camera.set(4,h)
 time.sleep(2)
 
 while (True):
     success, image = my_camera.read()
     image = cv2.flip(image,-1)
     image = cv2.GaussianBlur(image,(5,5),0)
-
+    
     image_HSV = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-    lower_pink = np.array([0,59,90])
-    upper_pink = np.array([1,255,255])
+    lower_pink = np.array([166,50,50])
+    upper_pink = np.array([174,255,255])
     mask = cv2.inRange(image_HSV,lower_pink,upper_pink)
     mask = cv2.GaussianBlur(mask,(5,5),0)
-
-    # findContours returns a list of the outlines of the white shapes in the mask (and a heirarchy that we shall ignore)            
-    contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
+    
+    # findContours returns a list of the outlines of the white shapes in the mask (and a heirarchy that we shall ignore)
+    image, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
     # If we have at least one contour, look through each one and pick the biggest
     if len(contours)>0:
         largest = 0
@@ -51,11 +52,11 @@ while (True):
         cv2.line(image,(target_x-2*diam,target_y),(target_x+2*diam,target_y),(0,255,0),1)
         cv2.line(image,(target_x,target_y-2*diam),(target_x,target_y+2*diam),(0,255,0),1)
     cv2.imshow('View',image)
- # Esc key to stop, otherwise repeat after 3 milliseconds
-    key_pressed = cv2.waitKey(3)
-    if key_pressed == 27:    
+    # Esc key to stop, otherwise repeat after 3 milliseconds
+key_pressed = cv2.waitKey(3)
+    if key_pressed == 27:
         break
-    
+
 cv2.destroyAllWindows()
 my_camera.release()
 
